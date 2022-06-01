85ed0d49adb49eab0c44e1a4856d62ebea18b4a7
Krista-Mari Katajisto <kristamari.katajisto@gmail.com>
2022-06-01 14:13:53 +0300

    New Component created

---
+++ src/app/servers/servers.component.ts
@@ -0,0 +1,39 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-servers',
+  // selector: '[app-servers]',
+  templateUrl: './servers.component.html',
+  // template: `
+  // <section>
+  // <h1>I'm in the Servers Component</h1>
+  // <p>Below this I have 2 Server Components</p>
+  // <app-server></app-server>
+  // <app-server></app-server>
+  // </section>
+  // `,
+  styleUrls: ['./servers.component.css']
+})
+export class ServersComponent implements OnInit {
+  allowNewServer:boolean = false;
+  serverCreationStatus = 'No server was created!';
+  serverName = 'TestServer';
+
+  constructor() {
+    setTimeout(() => {
+      this.allowNewServer = true;
+    }, 2000);
+   }
+
+  ngOnInit(): void {
+  }
+
+  onCreateServer () {
+    this.serverCreationStatus = 'Server was created! Server name is ' + this.serverName;
+  }
+
+  onUpdateServerName(event:Event) {
+    this.serverName = (<HTMLInputElement>event.target).value;
+  }
+
+}
