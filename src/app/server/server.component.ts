973566eb8b7bf248a91b4020af8a122e2f1862a3
Krista-Mari Katajisto <kristamari.katajisto@gmail.com>
2022-06-01 14:13:37 +0300

    Server component created and code added

---
+++ src/app/server/server.component.ts
@@ -0,0 +1,23 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-server',
+  // selector: '[app-server]',
+  // selector: '.app-server',
+  templateUrl: './server.component.html',
+  styleUrls: ['./server.component.css']
+})
+export class ServerComponent implements OnInit {
+  serverId: number = 19;
+  serverStatus: string = 'offline';
+
+  getServerStatus() {
+    return this.serverStatus;
+  }
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
